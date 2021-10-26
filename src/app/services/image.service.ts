7188529875c8a95552a50faf8e064447242a4ada
Stephen Kirkland <skirkland.job@gmail.com>
2021-10-26 05:14:03 -0400

    Working on color pallet.

--- src/app/services/image.service.ts
+++ src/app/services/image.service.ts
@@ -5,8 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class ImageService {
 
-  public flamingo: string = "assets/images/people/flamingo.jpg";
-  public profile: string = "assets/images/people/profile.jpg";
+  public profile: string = "assets/images/logos/luxy_nics_logo.jpg";
 
   constructor() { }
 }
